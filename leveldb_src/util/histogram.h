c56329e134c56f3c517fee8381186e2b5df77176
as2120 <126.org@gmail.com>
2014-08-28 15:52:41 +0800

    注释 Histogram
    2014-08-28 注释 Histogram

--- leveldb_src/util/histogram.h
+++ leveldb_src/util/histogram.h
@@ -7,36 +7,47 @@
 
 #include <string>
 
-namespace leveldb {
+namespace leveldb
+{
 
-class Histogram {
- public:
-  Histogram() { }
-  ~Histogram() { }
+//z 搜索了下代码，目前代码中没有使用该类
+class Histogram
+{
+public:
+	Histogram() { }
+	~Histogram() { }
 
-  void Clear();
-  void Add(double value);
-  void Merge(const Histogram& other);
+    //z 清除所有的值
+	void Clear();
+    //z 增加一个值
+	void Add(double value);
+    //z 合并两个直方图
+	void Merge(const Histogram& other);
 
-  std::string ToString() const;
+    //z 格式化输出直方图一些信息，如果均值，中位值，样本个数等等
+	std::string ToString() const;
 
- private:
-  double min_;
-  double max_;
-  double num_;
-  double sum_;
-  double sum_squares_;
+private:
+	double min_;
+	double max_;
+	double num_;
+	double sum_;
+	double sum_squares_;
 
-  enum { kNumBuckets = 154 };
-  static const double kBucketLimit[kNumBuckets];
-  double buckets_[kNumBuckets];
+	enum { kNumBuckets = 154 };
+	static const double kBucketLimit[kNumBuckets];
+    //z 存储 在对应范围的
+	double buckets_[kNumBuckets];
 
-  double Median() const;
-  double Percentile(double p) const;
-  double Average() const;
-  double StandardDeviation() const;
+    //z 中位数
+	double Median() const;
+	double Percentile(double p) const;
+    //z 平均值
+	double Average() const;
+    //z 方差
+	double StandardDeviation() const;
 };
 
 }
 
-#endif  // STORAGE_LEVELDB_UTIL_HISTOGRAM_H_
+#endif	// STORAGE_LEVELDB_UTIL_HISTOGRAM_H_
