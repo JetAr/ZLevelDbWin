5cca0e2e3a8d553a0bcd42c409b7fe434c4937a1
as2120 <126.org@gmail.com>
2014-08-28 17:59:00 +0800

    只是格式化了一下 block，未开始看代码
    只是格式化了一下 block，未开始看代码

--- leveldb_src/table/block.h
+++ leveldb_src/table/block.h
@@ -9,35 +9,40 @@
 #include <stdint.h>
 #include "leveldb/iterator.h"
 
-namespace leveldb {
+namespace leveldb
+{
 
 class Comparator;
 
-class Block {
- public:
-  // Initialize the block with the specified contents.
-  // Takes ownership of data[] and will delete[] it when done.
-  Block(const char* data, size_t size);
+class Block
+{
+public:
+	// Initialize the block with the specified contents.
+	// Takes ownership of data[] and will delete[] it when done.
+	Block(const char* data, size_t size);
 
-  ~Block();
+	~Block();
 
-  size_t size() const { return size_; }
-  Iterator* NewIterator(const Comparator* comparator);
+	size_t size() const
+	{
+		return size_;
+	}
+	Iterator* NewIterator(const Comparator* comparator);
 
- private:
-  uint32_t NumRestarts() const;
+private:
+	uint32_t NumRestarts() const;
 
-  const char* data_;
-  size_t size_;
-  uint32_t restart_offset_;     // Offset in data_ of restart array
+	const char* data_;
+	size_t size_;
+	uint32_t restart_offset_;	  // Offset in data_ of restart array
 
-  // No copying allowed
-  Block(const Block&);
-  void operator=(const Block&);
+	// No copying allowed
+	Block(const Block&);
+	void operator=(const Block&);
 
-  class Iter;
+	class Iter;
 };
 
 }
 
-#endif  // STORAGE_LEVELDB_TABLE_BLOCK_H_
+#endif	// STORAGE_LEVELDB_TABLE_BLOCK_H_
