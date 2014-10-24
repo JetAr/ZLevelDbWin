bae36d30035341ffb82db6adcd28c0ada3c9c9ca
as2120 <126.org@gmail.com>
2014-10-24 12:56:28 +0800

    10.24

--- leveldb_src/util/env.cc
+++ leveldb_src/util/env.cc
@@ -34,46 +34,62 @@ void Log(Logger* info_log, const char* format, ...) {
 }
 
 Status WriteStringToFile(Env* env, const Slice& data,
-                         const std::string& fname) {
-  WritableFile* file;
-  Status s = env->NewWritableFile(fname, &file);
-  if (!s.ok()) {
+                         const std::string& fname)
+{
+    //z 文件指针
+    WritableFile* file;
+    //z 根据文件名生成对应的WritableFile
+    Status s = env->NewWritableFile(fname, &file);
+    if (!s.ok())
+    {
+        return s;
+    }
+    //z 将数据追加到文件尾
+    s = file->Append(data);
+    if (s.ok())
+    {
+        //z 关闭文件
+        s = file->Close();
+    }
+    delete file;  // Will auto-close if we did not close above
+
+    //z 如果没有正常关闭，那么关闭文件。
+    if (!s.ok())
+    {
+        env->DeleteFile(fname);
+    }
     return s;
-  }
-  s = file->Append(data);
-  if (s.ok()) {
-    s = file->Close();
-  }
-  delete file;  // Will auto-close if we did not close above
-  if (!s.ok()) {
-    env->DeleteFile(fname);
-  }
-  return s;
 }
 
-Status ReadFileToString(Env* env, const std::string& fname, std::string* data) {
-  data->clear();
-  SequentialFile* file;
-  Status s = env->NewSequentialFile(fname, &file);
-  if (!s.ok()) {
+//z 将数据从文件fname中读取到data中。
+Status ReadFileToString(Env* env, const std::string& fname, std::string* data)
+{
+    data->clear();
+    SequentialFile* file;
+    Status s = env->NewSequentialFile(fname, &file);
+    if (!s.ok())
+    {
+        return s;
+    }
+    static const int kBufferSize = 8192;
+    char* space = new char[kBufferSize];
+    while (true)
+    {
+        Slice fragment;
+        s = file->Read(kBufferSize, &fragment, space);
+        if (!s.ok())
+        {
+            break;
+        }
+        data->append(fragment.data(), fragment.size());
+        if (fragment.empty())
+        {
+            break;
+        }
+    }
+    delete[] space;
+    delete file;
     return s;
-  }
-  static const int kBufferSize = 8192;
-  char* space = new char[kBufferSize];
-  while (true) {
-    Slice fragment;
-    s = file->Read(kBufferSize, &fragment, space);
-    if (!s.ok()) {
-      break;
-    }
-    data->append(fragment.data(), fragment.size());
-    if (fragment.empty()) {
-      break;
-    }
-  }
-  delete[] space;
-  delete file;
-  return s;
 }
 
 EnvWrapper::~EnvWrapper() {
